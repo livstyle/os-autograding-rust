f7806f2a411a3486ab28344004e765464e915616
姜坤 <gains9@sina.com>
2022-10-31 00:21:52 +0800

    action

---
+++ src/__tests__/.github/classroom/checkresult.js
@@ -0,0 +1,21 @@
+const restlings = require('rustlings');
+
+function judge(outputFile) {
+    let points = {};
+    let result = restlings.myverify();
+    // 保存到全局先
+    restlingsMyverify = result;
+    if (result !== 'error') {
+        let jsonResult = JSON.parse(result);
+        jsonResult.exercises.forEach(({ name, result }) => {
+            if (result) {
+                points[name] = [1,1]
+            } else {
+                points[name] = [0,1]
+            }
+        })
+    }
+    return points;
+}
+
+module.exports.judge = judge;
